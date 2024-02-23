90a07d6e6703a592fffbb516f50b2a393ef02ed2
eqt729 <iimhonde@gmail.com>
2024-02-23 16:09:20 -0500

    files added

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+
 int search(int numbers[], int low, int high, int value) 
 {
+	int mid;
+	if(low <= high){
+		mid = (low + high) /2;
+		if(value == numbers[mid])
+			return mid;
+		else if(value < numbers[mid]) 
+			return search(numbers, low, mid -1, value);
+
+			else	
+				return search(numbers, mid+ 1, high, value);
+		
+
+	}
+
 	return -1;
 }
 
